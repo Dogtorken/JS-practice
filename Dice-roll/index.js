a68dae90e1ead70c041c88475a78a19cc9f1c861
Kenward Terhemba <umaterhemba@gmail.com>
2024-03-27 13:38:12 +0100

    random number generator simulating a 3-dice throw from one to six

---
+++ Dice-roll/index.js
@@ -0,0 +1,21 @@
+let beforeClick = document.getElementsByClassName("rollBtn");
+
+let afterClick = beforeClick.cla
+
+const roll = document.getElementById("rollBtn");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+
+let min = 1;
+let max = 6;
+
+roll.onclick = function(){
+    let randomNum1 = Math.floor(Math.random() * max) + min;
+    let randomNum2 = Math.floor(Math.random() * max) + min;
+    let randomNum3 = Math.floor(Math.random() * max) + min;
+
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3
+}
